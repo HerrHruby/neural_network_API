cc1b89334993a783213cb4602b5adc588b00babd
ian <theplasmid2@gmail.com>
2020-10-31 15:32:54 +0000

    Added save data capability

--- main.py
+++ main.py
@@ -8,15 +8,11 @@ def main():
     # load in data and apply appropriate transformations
     training_data = np.load('data/fashion-train-imgs.npz')
     training_labels = np.load('data/fashion-train-labels.npz')
-    test_data = np.load('data/fashion-test-imgs.npz')
-    test_labels = np.load('data/fashion-test-labels.npz')
     validation_data = np.load('data/fashion-dev-imgs.npz')
     validation_labels = np.load('data/fashion-dev-labels.npz')
 
     training_data = np.reshape(training_data, (784, 12000)).T
     training_labels = np.reshape(training_labels, (-1, 1))
-    test_data = np.reshape(test_data, (784, 1000)).T
-    test_labels = np.reshape(test_labels, (-1, 1))
     validation_data = np.reshape(validation_data, (784, 1000)).T
     validation_labels = np.reshape(validation_labels, (-1, 1))
 
@@ -29,21 +25,10 @@ def main():
     model.add_layer('relu', 128, 32)
     model.add_layer('sigmoid', 32, 1)
     # compile and then fit to training data
-    model.compile(optimiser='momentum', loss='MSE', metric='binary_accuracy', learning_rate=0.01)
-    results = model.fit(input_data=training_data, labels=training_labels, epochs=5, batch_size=16, validate=True,
-                        validation_data=validation_data, validation_labels=validation_labels)
-
-    # code for running on test set
-    # total_loss = 0
-    # total_correct = 0
-    # for datapoint, label in zip(test_data, test_labels):
-    #     predictions = model.predict(datapoint)
-    #     loss = model.compute_loss(predictions, label, loss='MSE')
-    #     total_loss += loss
-    #     if round(predictions[0]) == label[0]:
-    #         total_correct += 1
-    # print('Test Loss: {}'.format(total_loss/len(test_data)))
-    # print('Test Accuracy: {}'.format(total_correct/len(test_data)))
+    model.set_name('128_32_1_test')
+    model.compile(optimiser='gradient_descent', loss='MSE', metric='binary_accuracy', learning_rate=0.01)
+    results = model.fit(input_data=training_data, labels=training_labels, epochs=50, batch_size=12, validate=True,
+                        validation_data=validation_data, validation_labels=validation_labels, save_data=True)
 
     # # code for hyperparameter tuning
     # tune_model = NN_api.Dense()  # initialise network and add layers
