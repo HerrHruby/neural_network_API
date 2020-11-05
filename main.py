4a9ec7f86e71b1e03a48160f13282e01ea00eb8f
ian <theplasmid2@gmail.com>
2020-11-05 14:18:31 +0800

    New default settings

--- main.py
+++ main.py
@@ -20,32 +20,30 @@ def main():
     # plt.show()
 
     model = NN_api.Dense()  # initialise network
-    # add layers (input: 784 -> hidden relu: 128 -> hidden relu: 32 -> output sigmoid: 1)
-    model.add_layer('relu', 784, 128)
-    model.add_layer('relu', 128, 32)
-    model.add_layer('sigmoid', 32, 1)
+    model.add_layer('relu', 784, 256)
+    model.add_layer('sigmoid', 256, 1)
     # compile and then fit to training data
-    model.set_name('128_32_1_test')
-    model.compile(optimiser='gradient_descent', loss='MSE', metric='binary_accuracy', learning_rate=0.01)
-    results = model.fit(input_data=training_data, labels=training_labels, epochs=50, batch_size=12, validate=True,
+    model.set_name('optimal_mom')
+    model.compile(optimiser='momentum', loss='MSE', metric='binary_accuracy',
+                  learning_rate=0.01, decay_rate=0.95)
+    results = model.fit(input_data=training_data, labels=training_labels, epochs=100, batch_size=120, validate=True,
                         validation_data=validation_data, validation_labels=validation_labels, save_data=True)
 
-    # # code for hyperparameter tuning
-    # tune_model = NN_api.Dense()  # initialise network and add layers
-    # tune_model.add_layer('relu', 784, 256)
-    # tune_model.add_layer('relu', 256, 32)
-    # tune_model.add_layer('relu', 32, 8)
-    # tune_model.add_layer('sigmoid', 8, 1)
-    #
-    # tuner = NN_api.GridSearchTuner()  # create tuner object
-    # tuner.set_model(tune_model)  # set model for tuner object
-    # tuner.learning_rate_grid([0.01, 0.1])  # provide grid for learning rate
-    # tuner.batch_size_grid([1, 12000])  # provide grid for batch size
-    # # perform grid search!
-    # tuner.search(training_data=training_data, labels=training_labels, validation_data=validation_data,
-    #              validation_labels=validation_labels, epochs=10, optimiser='gradient_descent', loss='MSE',
-    #              metric='binary_accuracy')
-    # print(tuner.get_best_params())
+    # code for hyperparameter tuning
+    tune_model = NN_api.Dense()  # initialise network and add layers
+    tune_model.add_layer('relu', 784, 256)
+    tune_model.add_layer('sigmoid', 256, 1)
+
+    tuner = NN_api.GridSearchTuner()  # create tuner object
+    tuner.set_model(tune_model)  # set model for tuner object
+    tuner.learning_rate_grid([0.00005, 0.0001, 0.0005, 0.001])  # provide grid for learning rate
+    tuner.batch_size_grid([1, 120, 1200, 12000])  # provide grid for batch size
+    tuner.decay_rates_grid([0.8, 0.85, 0.9, 0.95])
+    # perform grid search!
+    tuner.search(training_data=training_data, labels=training_labels, validation_data=validation_data,
+                 validation_labels=validation_labels, epochs=20, optimiser='momentum', loss='MSE',
+                 metric='binary_accuracy')
+    print(tuner.get_best_params())
 
 
 if __name__ == '__main__':
